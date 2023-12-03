ad20921c45c9774ee353738f031d65719d302f08
Langello <gastonchavo@gmail.com>
2023-12-03 19:22:32 -0300

    chore: Update package.json start script to use nodemon
    The start script in package.json has been modified to use 'nodemon'
    instead of 'node' for running the index.js file. This change enables
    automatic server restarts upon file changes, improving development
    workflow efficiency. No test specified in the script remains unchanged.
    
    A new file, dbVercel.js, has been added to the 'src' directory. This
    file establishes a connection to a PostgreSQL database hosted on Vercel.
    The connection utilizes SSL with disabled certificate verification.
    
    These changes enhance the development experience by enabling automatic
    server restarts and integrating the application with the Vercel-hosted
    database. No existing files have been modified.
    
    No issue references.

---
+++ back/src/dbVercel.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize( 'postgres://default:1SaFKRtWgw4d@ep-proud-king-11044566-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conectado a la base de datos');
+    // Aquí puedes realizar consultas y otras operaciones en la base de datos
+  } catch (error) {
+    console.error('Error al conectar a la base de datos', error);
+  }
+})();
+
+export { sequelize };
