7635b7966a093795722f0da6059f74f2ff809ecf
Langello <gastonchavo@gmail.com>
2023-12-03 12:44:18 -0300

    probando para deploy

--- back/src/index.js
+++ back/src/index.js
@@ -35,8 +35,7 @@ app.use(cors()); // Configurar CORS.
 app.use(express.json()); // Configuramos el middleware para analizar el cuerpo de las solicitudes con contenido JSON
 app.use(express.urlencoded({ extended: false })); // Configuramos el middleware para analizar el cuerpo de las solicitudes con contenido codificado en URL
 
-// Conexión a la base de datos
-autenticar();
+
 
 // Rutas
 app.get("/", getDocumentacion);
